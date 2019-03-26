41b627b11a725831778dcfd2d935615a6097251b
kyle smith <kylemclellan@gmail.com>
2019-03-26 20:33:31 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -14,7 +14,7 @@ module FancyDance
     end
     
     def take_a_bow
-      "Thank you, thank you. It was a peleasure to dance for you all."
+      "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
   
