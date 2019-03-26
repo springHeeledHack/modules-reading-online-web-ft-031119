35b7d9db6839011159bf8b944c65571e0f67230f
kyle smith <kylemclellan@gmail.com>
2019-03-26 20:13:29 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+  include Dance 
   
+  attr_accssor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
 end
